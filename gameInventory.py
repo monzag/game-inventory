bda1eeea654f38056273f8858e1010faa08cfefe
monzag <zaganiacz.m@gmail.com>
2017-04-28 22:39:57 +0200

    new gameInventory

--- gameInventory.py
+++ gameInventory.py
@@ -1,38 +1,97 @@
-# This is the file where you must work. Write code in the functions, create new functions, 
-# so they work according to the specification
+# Create game - inventory. Import data (string) from CSV, add to list and then to dictionary. 
+# Print inventory unordered, descending order or ascending order - depend on user. Export data to CSV. 
 
-# Displays the inventory.
+import csv
+
+inventory = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+added_items = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+
+
 def display_inventory(inventory):
-    pass
+    '''Count number of items'''
+    number = 0
+    for key, value in inventory.items():
+        number += value
+
+    print("Total number of items: ", number)
 
 
-# Adds to the inventory dictionary a list of items from added_items.
 def add_to_inventory(inventory, added_items):
-    pass
+    '''Adds to the inventory dictionary a list of items and count amount.'''
+    for item in added_items:
+        for key, value in inventory.items():
+            if item == key:
+                inventory[key] = value + 1
+
+        if item not in inventory.keys():
+            inventory[item] = 1
+
+    return inventory
 
 
-# Takes your inventory and displays it in a well-organized table with 
-# each column right-justified. The input argument is an order parameter (string)
-# which works as the following:
-# - None (by default) means the table is unordered
-# - "count,desc" means the table is ordered by count (of items in the inventory) 
-#   in descending order
-# - "count,asc" means the table is ordered by count in ascending order
 def print_table(inventory, order=None):
-    pass
+    '''Print table with inventory items.
+    3 options: unordered, descending order or ascending order. Depending on user input.'''
+    result = 0
+    for key in inventory.keys():
+        if len(key) > result:
+            result = len(key)
+
+    width = 3 + result
+    title1 = 'count'
+    title2 = 'item name'
+    print('Inventory:', '\n', title1.rjust(width), title2.rjust(width))
+    print(2*width*'-')
+
+    if order == ' ':
+        for key, value in inventory.items():
+            print(str(value).rjust(width), key.rjust(width))
+
+    if order == 'count,desc':
+        for word in sorted(inventory, key=inventory.get, reverse=True):
+            print(str(inventory[word]).rjust(width), word.rjust(width))
+
+    if order == 'count,asc':
+        for word in sorted(inventory, key=inventory.get, reverse=False):
+            print(str(inventory[word]).rjust(width), word.rjust(width))
+
+    print(2*width*'-')
+    display_inventory(inventory)
 
 
-# Imports new inventory items from a file
-# The filename comes as an argument, but by default it's 
-# "import_inventory.csv". The import automatically merges items by name.
-# The file format is plain text with comma separated values (CSV).
-def import_inventory(inventory, filename="import_inventory.csv"):
-    pass
+def import_inventory(inventory, filename='import_inventory.csv'):
+    '''Import data from CSV file. Change one string to list with elements.'''
+    with open(filename, encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile, delimiter='|', quoting=csv.QUOTE_NONE)
+        for row in reader:
+            for item in row:
+                new_list = item.split(',')
+
+        add_to_inventory(inventory, new_list)
 
 
-# Exports the inventory into a .csv file.
-# if the filename argument is None it creates and overwrites a file
-# called "export_inventory.csv". The file format is the same plain text 
-# with comma separated values (CSV).
-def export_inventory(inventory, filename="export_inventory.csv"):
-    pass
+def export_inventory(inventory, filename='export_inventory.csv'):
+    '''Export all inventory to new or default file. Change inventory from dictionary to long string separate comma'''
+    export_string = ''
+    for key, value in inventory.items():
+        new_key = key + ','
+        export_string += new_key * value
+    export = export_string[:-1]
+
+    with open(filename, 'w', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile, delimiter='|', quoting=csv.QUOTE_NONE)
+        writer.writerow([export])
+
+
+def main():
+    inv = add_to_inventory(inventory, added_items)
+    import_inventory(inv)
+    order = input('''
+    Push space - unordered,
+    write count,desc - descending order
+    write count,asc - ascending order: ''')
+    print_table(inv, order)
+    export_inventory(inv)
+
+if __name__ == '__main__':
+    main()
